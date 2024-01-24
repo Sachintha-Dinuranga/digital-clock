1fa70c1e7ad3e13049d110db6f23cef58b4ac776
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2024-01-24 08:42:03 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+function updateClock(){
+    const currentTime = new Date();
+    let hours = currentTime.getHours();
+    const meridiem = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0);
+    const minutes = currentTime.getMinutes().toString().padStart(2, 0);
+    const seconds = currentTime.getSeconds().toString().padStart(2, 0);
+    const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
+
+    document.getElementById("clock").textContent = timeString;
+
+
+}
+
+updateClock();
+setInterval(updateClock, 1000);
